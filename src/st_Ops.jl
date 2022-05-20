203e6ce995b66a016209852d7aa63bbf7fcaa994
Dongdong Kong <kongdd@users.noreply.github.com>
2022-05-20 23:33:19 +0800

    improve `st_Ops`

--- src/st_Ops.jl
+++ src/st_Ops.jl
@@ -1,15 +1,14 @@
 # Copyright (c) 2018 Maarten Pronk, MIT license
 # @references
 # 1. `https://github.com/evetion/GeoArrays.jl/blob/master/src/geoarray.jl`
-import Base.==
-
-
 # Base.IndexStyle(::Type{T}) where {T<:AbstractGeoArray} = IndexLinear()
 # Base.convert(::Type{Array{T, 3}}, A::AbstractGeoArray{T}) where {T} = convert(Array{T,3}, ga.A)
+# using Stars
+
 Base.iterate(ga::AbstractGeoArray) = iterate(ga.A)
 Base.length(ga::AbstractGeoArray) = length(ga.A)
 Base.parent(ga::AbstractGeoArray) = ga.A
-Base.map(f, ga::AbstractGeoArray) = GeoArray(map(f, ga.A), ga.f, ga.crs) # ?
+Base.map(f, ga::AbstractGeoArray) = GeoArray(ga, vals = map(f, ga.A))
 Base.size(ga::AbstractGeoArray) = size(ga.A)
 Base.eltype(::Type{AbstractGeoArray{T}}) where {T} = T
 
@@ -30,68 +29,41 @@ end
 Check whether two `GeoArrays`s `a` and `b` are geographically equal, 
 although not necessarily in content.
 """
-function equals(a::AbstractGeoArray, b::AbstractGeoArray)
-    size(a) == size(b) && a.f == b.f && a.crs == b.crs
+# function equals(a::AbstractGeoArray, b::AbstractGeoArray)
+#     size(a) == size(b) && a.f == b.f && a.crs == b.crs
+# end
+
+Base.:(==)(a::AbstractGeoArray, b::AbstractGeoArray) = size(a) == size(b) && a.f == b.f && a.crs == b.crs
+Base.:(==)(a::AbstractGeoArray, b::Real) = GeoArray(a, vals=a.A .== b)
+
+
+Base_funcs = ((:Base, :+), (:Base, :-), (:Base, :*), (:Base, :/),
+    (:Base, :>), (:Base, :<), (:Base, :>=), (:Base, :<=),
+    (:Base, :!=))
+for (m, f) in Base_funcs
+    # _f = Symbol(m, ".:", f)
+    @eval begin
+        $m.$f(a::AbstractGeoArray, b::AbstractGeoArray) = begin
+            a == b || throw(
+                DimensionMismatch("Can't operate on non-geographic-equal `AbstractGeoArray`s"))
+            GeoArray(a, vals=$m.$f.(a.A, b.A))
+        end
+
+        $m.$f(a::AbstractGeoArray, b::Real) = GeoArray(a, vals=$m.$f.(a.A, b))
+        $m.$f(a::Real, b::AbstractGeoArray) = GeoArray(a, vals=$m.$f.(a, b.A))
+    end
 end
 
-function ==(a::AbstractGeoArray, b::AbstractGeoArray)
-    size(a) == size(b) && a.f == b.f && a.crs == b.crs
-end
-
-function ==(a::AbstractGeoArray, b::Real)
-    GeoArray(a, vals=a.A .== b)
-end
-
-
-function Base.:-(a::AbstractGeoArray, b::AbstractGeoArray)
-    equals(a, b) || throw(DimensionMismatch("Can't operate on non-geographic-equal `AbstractGeoArray`s"))
-    GeoArray(a, vals=a.A .- b.A)
-end
-
-function Base.:+(a::AbstractGeoArray, b::AbstractGeoArray)
-    equals(a, b) || throw(DimensionMismatch("Can't operate on non-geographic-equal `AbstractGeoArray`s"))
-    GeoArray(a, vals=a.A .+ b.A)
-end
-
-function Base.:*(a::AbstractGeoArray, b::AbstractGeoArray)
-    equals(a, b) || throw(DimensionMismatch("Can't operate on non-geographic-equal `AbstractGeoArray`s"))
-    GeoArray(a, vals=a.A .* b.A)
-end
-
-function Base.:/(a::AbstractGeoArray, b::AbstractGeoArray)
-    equals(a, b) || throw(DimensionMismatch("Can't operate on non-geographic-equal `AbstractGeoArray`s"))
-    GeoArray(a, vals=a.A ./ b.A)
-end
-
-Base.:+(a::AbstractGeoArray, b::Real) = GeoArray(a, vals=a.A + b)
-Base.:-(a::AbstractGeoArray, b::Real) = GeoArray(a, vals=a.A - b)
-Base.:*(a::AbstractGeoArray, b::Real) = GeoArray(a, vals=a.A * b)
-Base.:/(a::AbstractGeoArray, b::Real) = GeoArray(a, vals=a.A / b)
-
-Base.isless(a::Real, ga::AbstractGeoArray) = rast(ga, vals=ga.A .> a)
-
-export -, +, *, /, isless, equals, isequal, ==
-
+# export -, +, *, /, isless, equals, isequal, ==
 
 ## Math OPERATIONS -------------------------------------------------------------
-function Base.:sum(ga::AbstractGeoArray, dims=3)
-    arr = sum(ga.A, dims=dims)
-    GeoArray(arr, ga.f, ga.crs)
-end
-
-function Base.:maximum(ga::AbstractGeoArray, dims=3)
-    arr = maximum(ga.A, dims=dims)
-    GeoArray(arr, ga.f, ga.crs)
-end
-
-function Base.:minimum(ga::AbstractGeoArray, dims=3)
-    arr = minimum(ga.A, dims=dims)
-    GeoArray(arr, ga.f, ga.crs)
-end
-
-function Statistics.:mean(ga::AbstractGeoArray, dims=3)
-    arr = mean(ga.A; dims=dims)
-    GeoArray(arr, ga.f, ga.crs)
+for (m, f) in ((:Base, :sum), (:Base, :maximum), (:Base, :minimum), (:Statistics, :mean))
+    @eval begin
+        $m.$f(ga::AbstractGeoArray; dims=3, kw...) = begin
+            arr = $m.$f(ga.A; dims=dims, kw...)
+            GeoArray(ga, vals=arr)
+        end
+    end
 end
 
 export sum, mean, maximum, minimum
